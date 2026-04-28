3abc3364db5a01d92a32f3b1843b91565d45efed
Gudule Lapointe <gudule@speculoos.world>
2026-04-27 22:16:35 -0400

    add phpcs PHPCompatibility check as composer dev dep and feature test

---
+++ tests/Features/CompatibilityTest.php
@@ -0,0 +1,9 @@
+<?php
+$appDir = dirname(__DIR__, 2);
+
+describe("Compatibility", function () use ($appDir) {
+	test("PHP 8.2 compatibility (phpcs PHPCompatibility)", function () use ($appDir) {
+		exec("$appDir/vendor/bin/phpcs 2>&1", $output, $code);
+		expect($code)->toBe(0, implode("\n", $output));
+	});
+});
