7534cd9786dc5ac6aaea4e54140cdd77adbc4109
Gudule Lapointe <gudule@speculoos.world>
2026-04-27 22:21:26 -0400

    phpcs: errors only in compatibility test (-n suppresses warnings)

--- tests/Features/CompatibilityTest.php
+++ tests/Features/CompatibilityTest.php
@@ -3,7 +3,7 @@ $appDir = dirname(__DIR__, 2);
 
 describe("Compatibility", function () use ($appDir) {
 	test("PHP 8.2 compatibility (phpcs PHPCompatibility)", function () use ($appDir) {
-		exec("$appDir/vendor/bin/phpcs 2>&1", $output, $code);
+		exec("$appDir/vendor/bin/phpcs -n 2>&1", $output, $code);
 		expect($code)->toBe(0, implode("\n", $output));
 	});
 });
