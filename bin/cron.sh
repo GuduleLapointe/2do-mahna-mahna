ad4140712687705dd27754fcc717ff64f8a23264
Gudule Lapointe <gudule@speculoos.world>
2026-05-08 17:23:43 -0400

    add bin/bash-init helper for bash scripts
    - centralize common init tasks
    - setup common vars (PGM, APP_DIR, LOG, OS, DEBUG, TRACCE, DRY_RUN,
      DATA_DIR
    - log() fail() and end() helper functioons
    - trap cleanup
    - update bin/cron.sh to source bin/bash-init

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Cron script for 2do-aggregator.
 #
@@ -13,85 +13,34 @@
 
 set -euo pipefail
 
-BASE_DIR=$(realpath $(dirname $0)/..)
-PGM=$(basename $0)
-TMP=/tmp/$PGM.$$
-LOG=$BASE_DIR/logs/$PGM.log
-OS=$(uname | tr [:upper:] [:lower:])
-DEBUG=${DEBUG:-}
-TRACE=${TRACE:-}
-DRY_RUN=${DRY_RUN:-}
+source $(dirname $0)/bash-init
 
-DATA_DIR=${DATA_DIR:-$BASE_DIR/data}
+log OS $OS
 
-for arg in "$@"; do
-    case "$arg" in
-        -n|--dry-run) DRY_RUN=1 ;;
-        -v|--verbose) DEBUG=1 ;;
-        -q|--quiet) DEBUG= ;;
-    esac
-done
-
-mkdir -p $BASE_DIR/logs
-
-log() {
-    echo "$@" >> $TMP.processing
-    error=
-    echo "$1" | cut -d " " -f 1 | grep -E -q '^[0-9]+$' && error=$1 && shift
-    [ "$error" = "0" ] && error=
-    [ -z "$error" -a -z "$DEBUG" ] && return
-    if [ "$error" ]; then
-        echo "$PGM [ERROR $error]: $@" >&2
-        return
-    fi
-    # [ -z "$DEBUG" ] && return
-    echo "$PGM: $@" >&2
-}
-
-fail() {
-    cat $TMP.processing >&2
-    echo "$1" | cut -d " " -f 1 | grep -E -q '^[0-9]+$' && error=$1 && shift || error=1
-    log $error "$@"
-    end $error
-}
-
-end() {
-    log $@ " - end processing, full log in $LOG"
-    mv $TMP.processing $LOG
-    [ "${tailpid:-}" ] && kill -9 $tailpid
-    exit $1
-}
-
-# sudo -nv 2>/dev/null || SUDO_GID="no_sudo"
 who=$(whoami)
-# [ "$os" = "darwin" ] && user=
-user=$(stat -c %U $0 2>/dev/null || stat -f %Su $0)
-if [ "$user" != "$who" ]
+[ "$OS" = "darwin" ] && owner=$(stat -f %Su $0) || owner=$(stat -c %U $0)
+if [ "$owner" != "$who" ]
 then
-    log "relaunching as $user"
-    sudo -u $user $0
+    log "relaunching as $owner"
+    sudo -u $owner "$0" "$*"
     exit $?
 fi
 log running as $who
 
 touch $TMP.processing
 varg=
-[ "$DEBUG" = "yes" -o "$DEBUG" = "true" -o "$DEBUG" = "1" ] && DEBUG=1 || DEBUG=
 if [ "$DEBUG" ]; then
     varg="-v"
     tail -f $TMP.processing &
     tailpid=$!
 fi
 
-[ "$TRACE" = "yes" -o "$TRACE" = "true" -o "$TRACE" = "1" ] && TRACE=1 && DEBUG=1 || TRACE=
-[ "$TRACE" ] && set -x
-
-cd $BASE_DIR || fail $? could not cd to $BASE_DIR
+cd $APP_DIR || fail $? could not cd to $APP_DIR
 [ -d $DATA_DIR ] || mkdir -p $DATA_DIR || fail $? could not create $DATA_DIR
 
 if [ -z "$DRY_RUN" ]; then
     log "starting aggregation"
-    $BASE_DIR/bin/aggregator.php $varg $DATA_DIR/ >> $TMP.processing 2>&1 || fail $? error while executing aggregator.php
+    $APP_DIR/bin/aggregator.php $varg $DATA_DIR/ >> $TMP.processing 2>&1 || fail $? error while executing aggregator.php
 else
     log "dry-run: skipping aggregation"
 fi
@@ -100,11 +49,11 @@ rsync_opts="-Waz"
 [ "$DRY_RUN" ] && rsync_opts="$rsync_opts --dry-run"
 
 errors=0
-[ -f $BASE_DIR/config/targets ] && grep . $BASE_DIR/config/targets | egrep -v "#|^\s*$" | while read target; do
+[ -f $APP_DIR/config/targets ] && sed -E "s:/+$::" $APP_DIR/config/targets | egrep -v "#|^\s*$" | while read target; do
     log updating $target/
     rsync $rsync_opts $DATA_DIR/ $target/ && continue
     log $? rsync to target $target failed
     errors=$((errors+1))
 done
 
-end $errors
+end $errors end processing
