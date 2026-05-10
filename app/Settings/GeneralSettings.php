918837f516dfb1a8c9b828c00c4bd4863d22e68f
Gudule Lapointe <gudule@speculoos.world>
2026-05-10 15:48:37 -0400

    add admin area and General Settings page

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -6,6 +6,8 @@ use Spatie\LaravelSettings\Settings;
 
 class GeneralSettings extends Settings
 {
+    public string $app_name;
+    public bool $site_active;
     public DateTimeZone $timezone;
 
     public static function group(): string
